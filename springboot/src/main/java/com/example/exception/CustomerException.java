fc75cff54ab40fb4fa22883ad2f76709f5dc11d6
真意 <15702225+zhen_yicr@user.noreply.gitee.com>
2025-09-02 18:02:43 +0800

    2

---
+++ springboot/src/main/java/com/example/exception/CustomerException.java
@@ -0,0 +1,39 @@
+package com.example.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice("com.example.exception")
+public class CustomerException extends RuntimeException {
+    private String code;
+    private String msg;
+
+    public CustomerException(String code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public CustomerException(String msg) {
+        this.code = "500";
+        this.msg = msg;
+    }
+
+    public CustomerException() {
+    }
+
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
---
+++ springboot/src/main/java/com/example/exception/GlobalExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.example.exception;
+
+import com.example.common.Result;
+import com.example.controller.WebController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+//全局异常处理
+@ControllerAdvice("com.example.exception")
+public class GlobalExceptionHandler {
+    private static final Logger log = LoggerFactory.getLogger(WebController.class);
+
+    @ExceptionHandler(Exception.class)
+    @ResponseBody  //将Result对象 转换成 json的格式
+    public Result error(Exception e) {
+        log.error("系统异常", e);
+        return Result.error("系统异常");
+    }
+
+
+    @ExceptionHandler(CustomerException.class)
+    @ResponseBody  //将Result对象 转换成 json的格式
+    public Result customererror(CustomerException e) {
+        log.error("自定义异常", e);
+        return Result.error(e.getCode(), e.getMsg());
+    }
+
+
+}
